9096049259fd65f5df3c1b6dfe97943b0183288c
ArpanSaha07 <sahaarpan550@gmail.com>
2022-08-22 19:04:10 +0600

    Breaking change.

--- project/main.py
+++ project/main.py
@@ -9,7 +9,7 @@ def main_wrapper():
     # Stuff here - wrapper!
     # project structuring for modularity, maintainability, and separation of concerns.
     # 2. git pull
-
+    This is a BREAKING CHANGE
     print("This is the end of our python project")
 
 
